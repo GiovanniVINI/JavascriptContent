fe116b22ff09643a3e02d09020174aa19cd23e76
vinicius_giovanni <viniciusmahssan@gmail.com>
2023-01-27 17:32:13 -0300

    doc: Adicionando novos arquivos :rocket:

---
+++ javaScript/Modulo1/Variaveis/lvl03/ex001.js
@@ -0,0 +1,15 @@
+// 1. Crie uma variável para armazenar o nome do carro e ponha o valor ferrari. E exiba-a.
+let car = 'ferrari'
+console.log(car)
+
+// 2. Crie uma variável para armazenar o preço do bolo, que custa R$ 19,99. E exiba-a.
+let valueCake = 19.99
+console.log(valueCake)
+
+/*
+    3. Corrija o código a seguir (e exiba-o).
+    let "cidade" = "São Paulo" 
+ 
+ */
+let cidade = "São Paulo"
+console.log(cidade)
---
+++ javaScript/Modulo1/Variaveis/lvl04/script.js
@@ -0,0 +1,9 @@
+// ---Quando usar ponto e vírtula---
+
+// Opcional
+var name = 'Naruto'
+console.log(name)
+
+// Obrigatório
+var name = 'Naruto'; let surname = 'Uzumaki'
+console.log(name, surname)
